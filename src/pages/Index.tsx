478d94ecb75f9f5e967467678b285b50b3672790
Digvijayb07 <digvijayb044@gmail.com>
2026-02-15 00:23:05 +0530

    changes

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,8 +1,6 @@
 import { Layout } from "@/components/layout/Layout";
 import { HeroSection } from "@/components/home/HeroSection";
 import { ServicesSection } from "@/components/home/ServicesSection";
-import { TestimonialsSection } from "@/components/home/TestimonialsSection";
-import { BlogSection } from "@/components/home/BlogSection";
 import { CTASection } from "@/components/home/CTASection";
 
 const Index = () => {
@@ -10,8 +8,6 @@ const Index = () => {
     <Layout>
       <HeroSection />
       <ServicesSection />
-      <TestimonialsSection />
-      <BlogSection />
       <CTASection />
     </Layout>
   );
